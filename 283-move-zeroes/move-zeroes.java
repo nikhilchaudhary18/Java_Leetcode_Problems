558a45bad6ff28e04191d99218e693adb2cc4ca8
Nikhil Chaudhary <nkcy7654@gmail.com>
2026-09-13 01:59:22 +0530

    Time: 232 ms (5.24%) | Memory: 48.1 MB (6.95%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,7 +5,7 @@ class Solution {
             noz++;
         }
         int n=arr.length;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<noz;i++){
             for(int j=0;j<n-1-i;j++){
                 if(arr[j]==0){
                     int temp = arr[j];
