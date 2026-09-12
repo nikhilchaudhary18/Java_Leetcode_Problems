f2134a6984d490fb0bc40469e84725c99221fc98
Nikhil Chaudhary <nkcy7654@gmail.com>
2026-09-12 12:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n<=2){
+            return n;
+        }
+        int a=1,b=2;
+        for(int i=3;i<=n;i++){
+            int sum = a+b;
+            a=b;
+            b=sum;
+        }
+        return b;
+    }
+}
