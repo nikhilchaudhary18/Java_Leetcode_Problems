02b1eb9769f21003a51b71731332f2a6efd82b3e
Nikhil Chaudhary <nkcy7654@gmail.com>
2026-09-12 12:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.31%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int a=0,b=1;
+        int sum=0;
+        for(int i=2;i<=n;i++){
+            sum = a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+        
+    }
+}
