95c05ef5b28012d7ab4845b2de2e01ad64ebf6f8
Nikhil Chaudhary <nkcy7654@gmail.com>
2026-09-13 23:13:53 +0530

    Time: 7 ms (28.82%) | Memory: 45.4 MB (21.10%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -3,12 +3,11 @@ class Solution {
         Arrays.sort(arr);
         int count =1;
         int max = arr[arr.length-1];
-        
         for(int i=arr.length-2;i>=0;i--){
             if(arr[i]!=arr[i+1]){
                 count++;
              if(count==3) {
-                return arr[i];
+                max = arr[i];
              }
             }
         }
