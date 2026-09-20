b0aae7061d9eb00cae4feed69eff51ff767a5d92
Nikhil Chaudhary <nkcy7654@gmail.com>
2026-09-20 11:29:39 +0530

    Time: 2 ms (96.85%) | Memory: 46.4 MB (27.03%) - LeetSync

--- 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.java
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.java
@@ -1,22 +1,21 @@
 class Solution {
     public int projectionArea(int[][] grid) {
+        int m =grid.length;
+        int n=grid[0].length;
         int top = 0;
         int front = 0;
         int side = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<m;i++){
             int maxi = 0;
             int maxj = 0;
-            for(int j=0;j<m;j++){
+            for(int j=0;j<n;j++){
                 maxi = Math.max(maxi,grid[i][j]);
                 maxj = Math.max(maxj,grid[j][i]);
-                if(grid[i][j]!=0){
-                    top++;
-                }
+                if(grid[i][j]!=0) top++;
+
             }
-            side += maxi;
-            front+=maxj;
+            front += maxi;
+            side += maxj;
         }
         return top+front+side;
     }
